919768beba85566664166d2a2d76660c1e2c562f
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-07 21:27:14 -0700

    Replace html grid with javascript DOM grid

--- javascript.js
+++ javascript.js
@@ -1,6 +1,22 @@
+const grid = document.querySelector('.grid-container');
+let n = 6;
+for (let i = 0; i < n; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row-container');
+    grid.appendChild(row);
+    for (let j = 0; j < n; j++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        row.appendChild(pixel);
+    }
+}
+
+
+
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach((pixel) => {
     pixel.addEventListener('mouseover', () => {
         pixel.style.backgroundColor='black';
     })
 });
+
