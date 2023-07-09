3a5607c7bfc085059a26969beaf37893b004685d
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-09 14:07:38 -0700

    Add slider value changes # of grid units

--- javascript.js
+++ javascript.js
@@ -1,17 +1,28 @@
-const grid = document.querySelector('.grid');
-let n = 5;
-for (let i = 0; i < n; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    grid.appendChild(row);
-    for (let j = 0; j < n; j++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        row.appendChild(pixel);
-    }
+// n = slidervalue
+let n = 16;
+updateGrid();
+var slider = document.getElementById("myRange");
+var slidervalue = document.querySelector('.slider-value');
+slidervalue.textContent = slider.value;
+slider.onchange = () => {
+    slidervalue.textContent = slider.value;
+    n = slider.value;
+    updateGrid();
 }
 
-
+function updateGrid() {
+    for (let i = 0; i < n; i++) {
+        const grid = document.querySelector('.grid');
+        const row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            row.appendChild(pixel);
+        }
+    }
+}
 
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach((pixel) => {
@@ -19,4 +30,3 @@ pixels.forEach((pixel) => {
         pixel.style.backgroundColor='black';
     })
 });
-
